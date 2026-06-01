9534f3bd26c66266800dc32b805dd1587c009c8a
Nikita Samusev <nikitf777@gmail.com>
2026-06-01 23:44:47 +0300

    Add the ability to hide buyers that don't have registered users

--- Controllers/BuyersController.cs
+++ Controllers/BuyersController.cs
@@ -1,5 +1,6 @@
 using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
+using Vkeram.Backend.Data.Repositories;
 using Vkeram.Backend.Services;
 
 namespace Vkeram.Backend.Controllers;
@@ -9,22 +10,33 @@ namespace Vkeram.Backend.Controllers;
 public class BuyersController : ControllerBase
 {
     private readonly IBuyersService _buyersService;
+    private readonly IUserRepository _userRepo;
     private readonly string _adminKey;
 
-    public BuyersController(IBuyersService buyersService, IConfiguration config)
+    public BuyersController(IBuyersService buyersService, IUserRepository userRepo, IConfiguration config)
     {
         _buyersService = buyersService;
+        _userRepo = userRepo;
         _adminKey = config["AdminApiKey"] ?? "";
     }
 
     [HttpGet]
     public async Task<ActionResult> GetBuyers(
-        [FromHeader(Name = "X-Admin-Key")] string adminKey)
+        [FromHeader(Name = "X-Admin-Key")] string adminKey,
+        [FromQuery] bool onlyWithUsers = false)
     {
         if (adminKey != _adminKey)
             return Unauthorized(new { Success = false, Message = "Invalid admin key." });
 
         var buyers = await _buyersService.GetAllAsync();
+
+        if (onlyWithUsers)
+        {
+            var users = await _userRepo.GetAllAsync();
+            var buyerIdsWithUsers = users.Select(u => u.BuyerId).ToHashSet();
+            buyers = buyers.Where(b => buyerIdsWithUsers.Contains(b.Id)).ToList();
+        }
+
         return Ok(new { Success = true, Buyers = buyers.Select(b => new { b.Id, b.Name }).ToList() });
     }
 
